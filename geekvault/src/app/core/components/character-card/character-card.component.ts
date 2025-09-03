53d234c60ed586ffd87423c9cb354548f6e9f6e5
Enmanuel Gomez Ventura <e.gomezv02@gmail.com>
2025-09-03 18:58:34 -0400

    Creacion base de detalles personajes.

--- geekvault/src/app/core/components/character-card/character-card.component.ts
+++ geekvault/src/app/core/components/character-card/character-card.component.ts
@@ -1,30 +1,12 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Character } from '../../models/character.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-character-card',
   standalone: true,
-  imports: [CommonModule],
-  styleUrls: ['./character-card.component.scss'],
-  template: `
-    <figure class="char-card" [attr.title]="character?.name">
-      <div class="img-wrap">
-        <img
-          [src]="character?.imageUrl || 'assets/placeholders/character-fallback.png'"
-          (error)="onError($event)"
-          [alt]="character?.name || 'Personaje'"
-          loading="lazy"
-        />
-      </div>
-      <figcaption class="char-name">{{ character?.name }}</figcaption>
-    </figure>
-  `
+  imports: [],
+  templateUrl: './character-card.component.html',
+  styleUrl: './character-card.component.scss'
 })
 export class CharacterCardComponent {
-  @Input() character!: Character;
 
-  onError(ev: Event) {
-    (ev.target as HTMLImageElement).src = 'assets/placeholders/character-fallback.png';
-  }
 }
